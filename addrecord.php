e1d14bf890b876452c0b381e2b50e8d95d654b42
Tan Phuc Le <tanphuc.le@gmail.com>
2017-07-29 18:03:44 +0700

    Add support for number of item & takeaway

--- addrecord.php
+++ addrecord.php
@@ -5,8 +5,11 @@
 	date_default_timezone_set('Asia/Ho_Chi_Minh');
 
 	$id = $_POST["id"];
+	$count = $_POST["count"];
+	$takeaway = $_POST["takeaway"];
+
 	$time = date('Y-m-d H:i:s');
-	$query = "INSERT INTO Records (itemID, time) VALUES($id, '$time')";
+	$query = "INSERT INTO Records (itemID, time, count, takeaway) VALUES($id, '$time', $count, $takeaway)";
 	echo $query;
 	$result = mysql_query($query);
 
